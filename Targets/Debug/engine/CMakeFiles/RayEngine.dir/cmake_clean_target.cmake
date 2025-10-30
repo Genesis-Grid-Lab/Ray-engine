affad7d46ab30da95bd51e623caa19a364d9e714
Nesmy Point du Jour <pnesmy@gmail.com>
2025-10-30 08:06:03 -0400

    windows init

--- Targets/Debug/engine/CMakeFiles/RayEngine.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../lib/libRayEngine.a"
-)
--- Targets/Debug/vendor/Raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nephilim/Projects/Ray-engine/vendor/Raylib
-# Build directory: /home/nephilim/Projects/Ray-engine/Targets/Debug/vendor/Raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
